3c9360f26f259b0fb2b5c8bf580ef9c2d9f8ff25
Dharra <sauravdharra@gmail.com>
2022-12-04 19:34:47 +0530

    done

---
+++ src/Data.js
@@ -0,0 +1,125 @@
+const PRODUCTS = [
+	{
+		id: 1,
+		name: '2k22 WWE New Editon Video Game',
+		price: '$12',
+		image: require  ('./images/1.jpg'),
+		
+		details:
+		"The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more." 
+	},
+	{
+		id: 2,
+		name: 'The Blood Line Tshirt',
+		price: '$14',
+		image: require('./images/2.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 3,
+		name: 'Becky Lynch Tshirt',
+		price: '$13.67',
+		image: require('./images/3.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 4,
+		name: 'WWE Championship Belt',
+		price: '$47.56',
+		image: require('./images/4.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 5,
+		name: 'WWE United States Championship Belt',
+		price: '$53.06',
+		image: require('./images/5.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 6,
+		name: 'Roman Reigns WWE Accessories',
+		price: '$93.20',
+		image: require('./images/6.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 7,
+		name: 'WWE Hoddies',
+		price: '$25',
+		image: require('./images/7.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 8,
+		name: 'Uncle Howdy TShirt',
+		price: '$99.98',
+		image: require('./images/8.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 9,
+		name: 'Ricochet TShirt',
+		price: '$87.22',
+		image: require("./images/9.jpg"),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 10,
+		name: 'Logan Paul TShirt',
+		price: '$11',
+		image: require('./images/10.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 11,
+		name: 'WWE products',
+		price: '$69',
+		image: require('./images/11.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 12,
+		name: 'Brother Of Destuction Belt',
+		price: '$33',
+		image: require('./images/12.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 13,
+		name: 'Jhon Cena Sweater',
+		price: '$99',
+		image: require('./images/13.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 14,
+		name: 'Jhon Cena Wrist band',
+		price: '$44',
+		image: require('./images/14.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+	{
+		id: 15,
+		name: 'Randy Orten Cap',
+		price: '$22.22',
+		image: require('./images/15.jpg'),
+		details:
+			'The WWE Shop is your online source for all things WWE including title belts, apparel and accessories. Get excited for the upcoming Survivor Series WarGames with newly-released WWE Survivor Series T-Shirts and more. ',
+	},
+];
+
+export default PRODUCTS;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+
+const Home = () => {
+  return (
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>Welcome to WWE Shop</h1>
+        </div>
+      </div>
+      <div className="container content">
+      <p>World Wrestling Entertainment, Inc., d/b/a as WWE, is an American professional wrestling promotion. A global integrated media and entertainment company, WWE has also branched out into other fields, including film, American football, and various other business ventures. The company is additionally involved in licensing its intellectual property to companies to produce video games and action figures.</p>
+      <p>The promotion was founded in 1953 as the Capitol Wrestling Corporation. It is the largest wrestling promotion in the world with its main roster divided up into two primary touring groups, along with a developmental roster based in Orlando, Florida (referred to by WWE as "brands"). Overall, WWE is available in more than 1 billion homes worldwide in 30 languages. The company's global headquarters is located in Stamford, Connecticut,[8] with offices in New York, Los Angeles, Mexico City, Mumbai, Shanghai, Singapore, Dubai and Munich.[9]
+
+      As in other professional wrestling promotions, WWE shows are not legitimate contests but entertainment-based performance theater, featuring story line-driven, scripted, and partially-choreographed matches; however, matches often include moves that can put performers at risk of injury, even death, if not performed correctly. The pre-determined aspect of professional wrestling was publicly acknowledged by WWE's owner Vince McMahon in 1989 in order to avoid taxes from athletic commissions. WWE brands its product as sports entertainment, acknowledging wrestling's roots in competitive sport and dramatic theater.</p>
+      </div>
+    </main>
+  )
+}
+
+export default Home
